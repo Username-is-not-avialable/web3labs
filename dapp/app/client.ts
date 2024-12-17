76efe4c73cb19c1904c70efe56231f1b143aed9c
username-is-not-avialable <antonbakir280@mail.ru>
2024-12-18 04:00:32 +0500

    lab7

---
+++ dapp/app/client.ts
@@ -0,0 +1,34 @@
+import { createWalletClient, createPublicClient, custom, http } from "viem";import { sepolia } from "viem/chains";
+import "viem/window";
+export function ConnectPublicClient() {
+ let transport;
+ if (window.ethereum) {
+ transport = custom(window.ethereum);
+ } else {
+ transport = http();
+ }
+
+ const publicClient = createPublicClient({
+ chain: sepolia,
+ transport: transport,
+ });
+
+ return publicClient;
+}
+export function ConnectWalletClient() {
+ let transport;
+ if (window.ethereum) {
+ // EIP-1193 Ethereum Provider JavaScript API
+ transport = custom(window.ethereum);
+ // or transport = http("https://eth.web3gate.ru:32443/e6c02775...")
+ } else {
+ const errorMessage ="Web3 wallet is not installed. Please install on throw new Error(errorMessage)";
+ }
+
+ const walletClient = createWalletClient({
+ chain: sepolia,
+ transport: transport,
+ });
+
+ return walletClient;
+}
---
+++ dapp/app/page.tsx
@@ -0,0 +1,14 @@
+import TokenComponent from "./tokenComponent";
+import TransactionComponent from "./transactionComponent";
+import WalletComponent from "./walletComponent";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <div className="flex flex-col items-center justify-center">
+        <WalletComponent />
+      </div>
+    </main>
+  );
+}
+
